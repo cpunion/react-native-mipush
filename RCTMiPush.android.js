64490ab7642508258bf7a9c72e7332102170e452
Li Jie <lijie@applean.cn>
2016-05-13 11:50:47 +0800

    Add MiPushClient methods

--- RCTMiPush.android.js
+++ RCTMiPush.android.js
@@ -9,10 +9,76 @@
 const MiPush = require('react-native').NativeModules.MiPush;
 
 var RCTMiPush = {
-  getInitialMessage: function(callback) {
-    callback = callback || function () {};
+  getInitialMessage: function() {
+    return MiPush.getInitialMessage();
+  },
 
-    MiPush.getInitialMessage(callback);
+  setAlias(alias, category) {
+    MiPush.setAlias(alias, category);
+  },
+
+  unsetAlias(alias, category) {
+    MiPush.unsetAlias(alias, category);
+  },
+
+  setUserAccount(userAccount, category) {
+    MiPush.setUserAccount(userAccount, category);
+  },
+
+  unsetUserAccount(userAccount, category) {
+    MiPush.unsetUserAccount(userAccount, category);
+  },
+
+  subscribe(topic, category) {
+    MiPush.subscribe(topic, category);
+  },
+
+  unsubscribe(topic, category) {
+    MiPush.unsubscribe(topic, category);
+  },
+
+  pausePush(category) {
+    MiPush.pausePush(category);
+  },
+
+  resumePush(category) {
+    MiPush.resumePush(category);
+  },
+
+  setAcceptTime(startHour, startMin, entHour, endMin, category) {
+    MiPush.setAcceptTime(startHour, startMin, entHour, endMin, category);
+  },
+
+  getAllAlias() {
+    return MiPush.getAllAlias();
+  },
+
+  getAllTopics() {
+    return MiPush.getAllTopics();
+  },
+
+  reportMessageClicked(msgId) {
+    MiPush.reportMessageClicked(msgId);
+  },
+
+  clearNotification(notifyId) {
+    MiPush.clearNotification(notifyId);
+  },
+
+  clearAllNotification() {
+    MiPush.clearAllNotification();
+  },
+
+  setLocalNotificationType(notifyType) {
+    MiPush.setLocalNotificationType(notifyType);
+  },
+
+  clearLocalNotificationType() {
+    MiPush.clearLocalNotificationType();
+  },
+
+  getRegId() {
+    return MiPush.getRegId();
   }
 };
 
--- android/app/src/main/java/com/xiaomi/push/reactnative/MiPushModule.java
+++ android/app/src/main/java/com/xiaomi/push/reactnative/MiPushModule.java
@@ -5,24 +5,27 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Process;
 import android.util.Log;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.xiaomi.mipush.sdk.PushMessageHelper;
 import com.xiaomi.mipush.sdk.MiPushMessage;
+import com.xiaomi.mipush.sdk.MiPushClient;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.Arrays;
 
@@ -79,13 +82,102 @@ public class MiPushModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getInitialMessage(Callback callback) {
+    public void getInitialMessage(Promise promise) {
         WritableMap params = getDataOfIntent(mIntent);
         // Add missing NOTIFICATION_MESSAGE_CLICKED message in PushMessageReceiver
         if (params != null) {
             params.putString("type", "NOTIFICATION_MESSAGE_CLICKED");
         }
-        callback.invoke(params);
+        promise.resolve(params);
+    }
+
+    @ReactMethod
+    public void setAlias(String alias, String category) {
+        MiPushClient.setAlias(getReactApplicationContext(), alias, category);
+    }
+
+    @ReactMethod
+    public void unsetAlias(String alias, String category) {
+        MiPushClient.unsetAlias(getReactApplicationContext(), alias, category);
+    }
+
+    @ReactMethod
+    public void setUserAccount(String userAccount, String category) {
+        MiPushClient.setUserAccount(getReactApplicationContext(), userAccount, category);
+    }
+
+    @ReactMethod
+    public void unsetUserAccount(String userAccount, String category) {
+        MiPushClient.unsetUserAccount(getReactApplicationContext(), userAccount, category);
+    }
+
+    @ReactMethod
+    public void subscribe(String topic, String category) {
+        MiPushClient.subscribe(getReactApplicationContext(), topic, category);
+    }
+
+    @ReactMethod
+    public void unsubscribe(String topic, String category) {
+        MiPushClient.unsubscribe(getReactApplicationContext(), topic, category);
+    }
+
+    @ReactMethod
+    public void pausePush(String category) {
+        MiPushClient.pausePush(getReactApplicationContext(), category);
+    }
+
+    @ReactMethod
+    public void resumePush(String category) {
+        MiPushClient.resumePush(getReactApplicationContext(), category);
+    }
+
+    @ReactMethod
+    public void setAcceptTime(int startHour, int startMin, int endHour, int endMin, String category) {
+        MiPushClient.setAcceptTime(getReactApplicationContext(), startHour, startMin, endHour, endMin, category);
+    }
+
+    @ReactMethod
+    public void getAllAlias(Promise promise) {
+        List<String> allAlias = MiPushClient.getAllAlias(getReactApplicationContext());
+        String[] allAliasArray = allAlias.toArray(new String[allAlias.size()]);
+        promise.resolve(Arguments.fromArray(allAliasArray));
+    }
+
+    @ReactMethod
+    public void getAllTopics(Promise promise) {
+        List<String> allTopics = MiPushClient.getAllAlias(getReactApplicationContext());
+        String[] allTopicsArray = allTopics.toArray(new String[allTopics.size()]);
+        promise.resolve(Arguments.fromArray(allTopicsArray));
+    }
+
+    @ReactMethod
+    public void reportMessageClicked(String msgId) {
+        MiPushClient.reportMessageClicked(getReactApplicationContext(), msgId);
+    }
+
+    @ReactMethod
+    public void clearNotification(int notifyId) {
+        MiPushClient.clearNotification(getReactApplicationContext(), notifyId);
+    }
+
+    @ReactMethod
+    public void clearAllNotification() {
+        MiPushClient.clearNotification(getReactApplicationContext());
+    }
+
+    @ReactMethod
+    public void setLocalNotificationType(int notifyType) {
+        MiPushClient.setLocalNotificationType(getReactApplicationContext(), notifyType);
+    }
+
+    @ReactMethod
+    public void clearLocalNotificationType() {
+        MiPushClient.clearLocalNotificationType(getReactApplicationContext());
+    }
+
+    @ReactMethod
+    public void getRegId(Promise promise) {
+        MiPushClient.getRegId(getReactApplicationContext());
     }
 
     public void onIntent(Intent intent) {
